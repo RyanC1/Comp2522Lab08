407807c7e964306ca4d65b42ba7e28e8c6ab0240
Ted Ip   <hip7@my.bcit.ca>
2025-03-11 13:02:14 -0700

    added a filteredstream method filtering out null and blank data. q6, q7 done

--- src/code/CountryLab.java
+++ src/code/CountryLab.java
@@ -3,15 +3,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
 
-public class CountryLab
+public class  CountryLab
 {
     //TODO make a function to add a stream of strings into a txt file
     //TODO for now just end each stream with foreach(System.out::println) to see the stream
+
+    /**
+     * Reusable method to get non-null stream
+     * */
+    private static Stream<String> filteredStream (final List<String> listToBeFiltered)
+    {
+        return listToBeFiltered.stream().filter(p-> p != null && !p.isBlank());
+    }
+
 
     public static void main(final String[] args) throws
                                                  IOException
@@ -53,5 +63,28 @@ public class CountryLab
 
         //this is an example of what we are doing
         countries.stream().forEach(System.out::println);
+
+        /***
+         * 6. Sorted Names (Ascending): List all country names in alphabetical order.
+         */
+        System.out.println("\n6. Country names in ascending order:");
+        final List<String> filteredStream6 = filteredStream(countries)
+                .sorted()
+                .toList();
+        filteredStream6.forEach(System.out::println);
+
+        /***
+         * 7. Sorted Names (Descending): List all country names in reverse alphabetical order.
+         */
+        System.out.println("\n7. Country names in descending order:");
+        final List<String> filteredStream7 = filteredStream(countries)
+                .sorted(Comparator.reverseOrder())
+                .toList();
+        filteredStream7.forEach(System.out::println);
+
+
+
+
+
     }
 }
